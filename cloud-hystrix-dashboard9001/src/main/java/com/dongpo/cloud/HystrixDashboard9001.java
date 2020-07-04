1321308af1a5f78c439eeec4cc86ef36ea6fccdb
dongpo <1520436215@qq.com>
2020-07-04 21:59:37 +0800

    endding the hystrix and hystrix dashboard.

---
+++ cloud-hystrix-dashboard9001/src/main/java/com/dongpo/cloud/HystrixDashboard9001.java
@@ -0,0 +1,11 @@
+package com.dongpo.cloud;
+
+/**
+ * @className HystrixDashboard9001
+ * @Description: TODO
+ * @Author dongpo
+ * @Date 2020/7/4
+ * @Version V1.0
+ **/
+public class HystrixDashboard9001 {
+}
